0bc8f054ab7522ef4c6d67396a5a0d357a9d6dde
sclement2 <sclement2@gmail.com>
2024-04-06 01:51:30 +0000

    Finish Receipts for Lovely Loveseats

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
+# Sales tax 
 sales_tax = 0.088
 
+# Catalog
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 lovely_loveseat_price = 254.00
 stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
@@ -7,21 +9,26 @@ stylish_settee_price = 189.50
 luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
 luxurious_lamp_price = 52.15
 
+# Variables for customer receipt
 customer_one_total = 0
 customer_one_itemization = ""
 customer_one_tax = 0
 
+# Add customer order to receipt 
 customer_one_total += lovely_loveseat_price
-customer_one_itemization += lovely_loveseat_description
+customer_one_itemization += "-" + lovely_loveseat_description
 
 customer_one_total += luxurious_lamp_price
 customer_one_itemization += "\n"
-customer_one_itemization += luxurious_lamp_description
+customer_one_itemization += "-" + luxurious_lamp_description
 
+# Tax calculation
 customer_one_tax = sales_tax * customer_one_total
-
 customer_one_total += customer_one_tax
 
-print(customer_one_total)
+# Customer receipt
+print("Customer One Items:")
 print(customer_one_itemization)
-print("1" + "\n" + "2")
+
+print("Customer One Total:")
+print(f"{customer_one_total:.2f}")
