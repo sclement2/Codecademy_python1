909b150ae6471070ea371281d9d058661dc6e3b8
sclement2 <sclement2@gmail.com>
2024-04-06 09:15:58 +0000

    Update to include Codecademy function section

--- travel.py
+++ travel.py
@@ -1,5 +1,6 @@
-def trip_welcome():
+def trip_welcome(origin, destination):
     print("Welcome to Tripcademy!")
+    print("Looks like you are traveling from " + origin + " to " + destination)
     print("Let's get you to your destination.")
 
 
@@ -19,7 +20,34 @@ def generate_trip_instructions(location):
     print("You can use the public subway system to get to " + location)
 
 
-generate_trip_instructions("Central Park")
-generate_trip_instructions("Grand Central Station")
+def calculate_expenses(plane_ticket_price, car_rental_rate, hotel_rate, trip_time):
+    car_rental_total = car_rental_rate * trip_time
+    hotel_total = hotel_rate * trip_time - 10
+
+    trip_total = car_rental_total + hotel_total + plane_ticket_price
+    return trip_total
+
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0 " + name)
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport = "Car"):
+  print("Your trip starts off in " + origin)
+  print("And you are traveling to " + destination)
+  print("You will be traveling by " + mode_of_transport)
+  print("It will take approximately " + str(estimated_time) + " hours")
+
+# Step 5: call your function
+print(calculate_expenses(200, 100, 100, 5))
+
+trip_planner_welcome("Johnny")
+estimate = estimated_time_rounded(14.5)
+destination_setup("Paris", "Rome", estimate, "train")
+
+# generate_trip_instructions("Central Park")
+# generate_trip_instructions("Grand Central Station")
 
 # directions_to_timesSq()
